c44834b2a3bbe9ef6bed1236a76ab9ebd4cc7692
MartonBrandon03 <145388980+MartonBrandon03@users.noreply.github.com>
2024-02-18 01:09:07 -0600

    add game logic and play again button

--- app.js
+++ app.js
@@ -1,63 +1,111 @@
 const start = document.querySelector('.start');
 const main = document.querySelector('main');
+const container = document.querySelector('.container');
+const counter = document.querySelector('.counter');
+const dialog = document.querySelector('.dialog');
+const match1 = document.querySelector('.match1');
+const match2 = document.querySelector('.match2');
+const playAgain = document.querySelector('.play-again');
+let humanCounter = 0;
+let computerCounter = 0;
 start.addEventListener('click',e=>{
   start.classList.toggle('hidden',true);
   main.classList.toggle('hidden',false);
 });
 function getComputerChoice(){
-  let rng = Math.floor(Math.random()*3)
-  let choice;
+  let rng = Math.floor(Math.random()*3);
   switch (rng) {
     case 0
-    :choice = "rock"
-    break;
+    :return "rock";
     case 1
-    :choice = "paper"
-    break;
+    :return "paper";
     case 2
-    :choice = "scissors"
+    :return "scissors";
+  }
+}
+function updateCounter(){
+  if(humanCounter === 5){
+    dialog.innerText = 'You win!';
+    container.classList.toggle('hidden',true);
+    playAgain.classList.toggle('hidden',false);
+  }else if(computerCounter === 5){
+    dialog.innerText = 'You lose!';
+    container.classList.toggle('hidden',true);
+    playAgain.classList.toggle('hidden',false);
+  }
+  counter.innerText = `Human:${humanCounter} Computer:${computerCounter}`;
+}
+playAgain.addEventListener('click',()=>{
+  humanCounter = 0;
+  computerCounter = 0;
+  container.classList.toggle('hidden',false);
+  playAgain.classList.toggle('hidden',true);
+  dialog.innerText = 'Choose';
+  match1.innerText = '';
+  match2.innerText = '';
+  updateCounter();
+});
+container.addEventListener('click',e=>{
+  switch(e.target.id){
+    case 'rock'
+    :playRound('rock',getComputerChoice());
+    updateCounter()
+    break;
+    case 'paper'
+    :playRound('paper',getComputerChoice());
+    updateCounter();
+    break;
+    case 'scissors'
+    :playRound('scissors',getComputerChoice());
+    updateCounter();
     break;
   }
-  return choice
-}
+});
+
 function playRound(playerSelection, computerSelection) {
-  let rockRegex = /rock/gi
-  let paperRegex = /paper/gi
-  let scissorsRegex = /scissors/gi
-  if(playerSelection.match(rockRegex)){
+  if(playerSelection === 'rock'){
+    match1.innerText = 'Rock';
     switch(computerSelection){
       case "rock"
-      :return "tie"
-      break
+      :match2.innerText = 'Rock';
+      break;
       case "paper"
-      :return "u lose"
+      :match2.innerText = 'Paper';
+      computerCounter++;
       break
       case "scissors"
-      :return "u win"
-      break
+      :match2.innerText = 'Scissors';
+      humanCounter++;
+      break;
     }
-  }else if(playerSelection.match(paperRegex)){
+  }else if(playerSelection === 'paper'){
+    match1.innerText = 'Paper';
     switch(computerSelection){
       case "rock"
-      :return "u win"
-      break
+      :match2.innerText = 'Rock';
+      humanCounter++;
+      break;
       case "paper"
-      :return "tie"
-      break
+      :match2.innerText = 'Paper';
+      break;
       case "scissors"
-      :return "u lose"
+      :match2.innerText = 'Scissors';
+      computerCounter++;
     }
   }else{
+    match1.innerText = 'Scissors';
     switch(computerSelection){
       case "rock"
-      :return "u lose"
-      break
+      :match2.innerText = 'Rock';
+      computerCounter++;
+      break;
       case "paper"
-      :return "u win"
-      break
+      :match2.innerText = 'Paper'
+      humanCounter++;
+      break;
       case "scissors"
-      :return "tie"
-      break
+      :match2.innerText = 'Scissors';
+      break;
     }
   }
 }
